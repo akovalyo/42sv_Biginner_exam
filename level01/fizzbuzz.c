6c70244564c63e9076573106f92373859ebbec8e
akovalyo <al.kovalyov@gmail.com>
2020-02-09 17:58:59 -0800

    upd l00, l01

---
+++ level01/fizzbuzz.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovalyo <al.kovalyov@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/09 16:58:57 by akovalyo          #+#    #+#             */
+/*   Updated: 2020/02/09 17:14:21 by akovalyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nbr)
+{
+	if (nbr < 0)
+	{	
+		write(1, "-", 1);
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		ft_putnbr(nbr/10);
+	nbr = nbr % 10 + '0';
+	write(1, &nbr, 1);
+}
+
+int	main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
